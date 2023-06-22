7f80d3851f0016aacd7c3f9aac91f1df7d72949d
Zakaria Makhkhas <zmakhkha@e1r9p9.1337.ma>
2023-06-22 09:09:05 +0100

    C Memory leaks checker

---
+++ C_leaks_checker/index.py
@@ -0,0 +1,45 @@
+# to replace in header
+'''
+static void *_malloc(size_t c, char *file, int line)
+{
+	void *p = malloc(c);
+	printf("MALC: %p, LINE = %i, FILE = %s\n", p, line, file);
+	return (p);
+}
+
+static void _free(void* c, char *file, int line)
+{
+	printf("FREE: %p, LINE = %i, FILE = %s\n", c, line, file);
+	return (free(c));
+}
+'''
+
+allocated = {}
+freed = {}
+
+# Read input from a file
+filename = 'leaks'
+with open(filename, 'r') as file:
+    file_content = file.read()
+
+for line in file_content.split('\n'):
+    line = line.strip()
+    if not line:
+        continue
+    if not (line.startswith("FREE") or line.startswith("MALC")):
+        continue
+    pointer = line.split(' ')[1][:-1]
+    _type = line.split(' ')[0][:-1]
+
+    if _type == "FREE":
+        freed[pointer] = 1
+    if _type == "MALC":
+        allocated[pointer] = line
+
+
+for key in freed:
+    allocated[key] = 0
+
+for key, value in allocated.items():
+    if value != 0:
+        print(value)
